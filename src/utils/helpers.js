b49569a3241062c35b7f40264c3b0f93fc515e3d
Riyansyah <riyansyah.codes@gmail.com>
2026-02-09 14:14:47 +0700

    color updt

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 export const getTierColor = (tier) => {
   switch(tier) {
-    case 'Platinum': return 'text-purple-600';
+    case 'Platinum': return 'text-[#c7a96a]';
     case 'Gold': return 'text-yellow-600';
     case 'Silver': return 'text-gray-500';
     default: return 'text-blue-600';
